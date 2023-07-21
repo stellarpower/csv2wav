f214eef33ee79289e28eca4ede995064e8d44b60
stellarpower <stellarpower@googlemail.com>
2023-07-21 15:47:02 +0100

    Divide samples through by minimum/maximum - not completely sure on the format, but the original code and exampleso n the wavefle website are bounded within (-1,+1) so we will stick with this.

--- classes/simple_csv2_wav.rb
+++ classes/simple_csv2_wav.rb
@@ -9,13 +9,20 @@ class SimpleCsv2Wav
     
     WaveFile::Writer.new(outfile, WaveFile::Format.new(:mono, :pcm_16, samples_per_sec)) do |writer|
       read_format = WaveFile::Format.new(:mono, :float, samples_per_sec)
-      
-      allSamples = io_in.readlines[headerRowCount..-1].map {|row|
+
+      ## We can't just write floats, as we go beyond the boundaries(?)
+      ## So we need to get the maximum value, or a high/low limit (μV)
+      ## and divide by this, so that the values are bounded between +1/-1
+
+      allSamples = io_in.readlines[headerRowCount..-1].map{|row|
         col = row.split(',')[col_nr].chomp
         num = col.to_f
       }
-      allSamples.each {|num|
-        buffer = WaveFile::Buffer.new([num], read_format)
+
+      maximum = allSamples.map{|sample| sample.abs}.max # Include minimum by getting the absolute.
+
+      allSamples.each { |num|
+        buffer = WaveFile::Buffer.new([num / maximum], read_format)
         writer.write(buffer)
       }
     end
