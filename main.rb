070509d9b3ff237bb3408d0012664fbdea0af4da
stellarpower <stellarpower@googlemail.com>
2023-07-21 15:43:14 +0100

    Add parameter for the number of rows considered as CSV header to exclude from the file.

--- main.rb
+++ main.rb
@@ -3,9 +3,9 @@
 
 require_relative './classes/simple_csv2_wav'
 
-infile, col_nr, outfile, samples_per_sec = *ARGV
+infile, col_nr, outfile, samples_per_sec, headerRowCount  = *ARGV
 
 File.open(infile, 'r') do |io_in|
-  SimpleCsv2Wav.new.convert(io_in, col_nr.to_i, outfile, samples_per_sec.to_i)
+  SimpleCsv2Wav.new.convert(io_in, col_nr.to_i, samples_per_sec.to_i, headerRowCount.to_i)
 end
 
