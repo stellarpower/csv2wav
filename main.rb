e46dacb66a0f22b492b505c3d5165a0ba26f5237
stellarpower <stellarpower@googlemail.com>
2023-07-21 16:13:38 +0100

    Convert to FLAC in lieu of resampling the WAV - seems FLAC can handle the low sample rate and will interpolate whilst playing; whilst buffer underun etc. were detected with the WAV.

--- main.rb
+++ main.rb
@@ -6,12 +6,12 @@ require_relative './classes/simple_csv2_wav'
 infile, col_nr, outfile, samples_per_sec, headerRowCount = *ARGV
 
 TempFile = "/tmp/CSV.wav"
-OutputSampleRate = 44100
 
 File.open(infile, 'r') do |io_in|
   SimpleCsv2Wav.new.convert(io_in, col_nr.to_i, TempFile, samples_per_sec.to_i, headerRowCount.to_i)
 end
 
 
-# Now to resample. E.g. 128 Hz seems to be valid but unlikely to play well
-`ffmpeg -i #{TempFile} -ar #{OutputSampleRate} #{outfile}`
+# Suggest using FLAC, as low sample rates will be better-interpolated, whils no data will be lost.
+`ffmpeg -i #{TempFile} #{outfile}`
+
