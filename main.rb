f86195ecc569a32db3721ed4a7b65b1ea9b93cc2
stellarpower <stellarpower@googlemail.com>
2023-07-21 17:20:02 +0100

    Change back to resampling output audio by default, to enable playing on mobile devices.

--- main.rb
+++ main.rb
@@ -6,12 +6,16 @@ require_relative './classes/simple_csv2_wav'
 infile, col_nr, outfile, samples_per_sec, headerRowCount = *ARGV
 
 TempFile = "/tmp/CSV.wav"
+OutputSampleRate = 44100
 
 File.open(infile, 'r') do |io_in|
   SimpleCsv2Wav.new.convert(io_in, col_nr.to_i, TempFile, samples_per_sec.to_i, headerRowCount.to_i)
 end
 
 
-# Suggest using FLAC, as low sample rates will be better-interpolated, whils no data will be lost.
-`ffmpeg -i #{TempFile} #{outfile}`
+# # Can use FLAC, as low sample rates will be better-interpolated, whils no data will be lost.
+# `ffmpeg -i #{TempFile}                         #{outfile}`
+# However, this is not playable on all devices, so we resample. FLAC is suggested, and will be autodetected by ffmpeg from the `outfile` argument.
+  `ffmpeg -i #{TempFile} -ar #{OutputSampleRate} #{outfile}`
+
 
